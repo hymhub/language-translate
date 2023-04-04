4588cb0ae6b25aee309e15b0dbba3612fc72962d
xm <1749207188@qq.com>
2023-04-04 23:58:30 +0800

    add fast mode example

--- example/baidu_api_test/translate.config.js
+++ example/baidu_api_test/translate.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
   fromPath: 'locales/*.en.json',
   apiKeyConfig: {
     type: TranslateService.baidu,
+    // 免费版限制请求频率, 非常鸡肋, 非要用百度建议使用付费高级版
     [TranslateService.baidu]: {
       appId: 'your appId',
       appKey: 'your appKey'
